51642f58fb3921013c3ffe40033ae03eb4fdac4c
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2023-10-21 17:01:41 +0700

    Update program files

---
+++ NomenklatureApi/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace NomenklatureApi
+{
+    #region Public Class AppSettings
+
+    /// <summary>
+    /// Класс для хранения строки подключения к БД
+    /// </summary>
+    public class AppSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+    #endregion
+}
--- NomenklatureApi/Program.cs
+++ NomenklatureApi/Program.cs
@@ -1,23 +1,55 @@
-var builder = WebApplication.CreateBuilder(args);
+#region Using
+using Microsoft.EntityFrameworkCore;
+using NomenklatureApi.Data;
+#endregion
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace NomenklatureApi
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    #region Public Class Program
+
+    /// <summary>
+    /// Основной класс api
+    /// </summary>
+    public class Program
+    {
+        #region Public Static Methods
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddDbContext<NomenklatureContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionString")));
+
+            builder.Services.AddTransient<INomenklatureRepository, NomenklatureRepository>();
+
+            var app = builder.Build();
+
+            using(var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<NomenklatureContext>();
+
+                db.Database.Migrate();
+            }
+
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+        #endregion
+    }
+    #endregion
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
